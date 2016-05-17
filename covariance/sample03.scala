01daa7cde4a97ffd355133d4a82f8429a9f16c2c
Hyungsok Lee <hyungsok7@gmail.com>
2016-05-17 21:01:17 +0900

    Create sample03.scala

---
+++ covariance/sample03.scala
@@ -0,0 +1,18 @@
+// List[Nothing] <: List[Something] 공변성 [+T]
+// List[Something] <: List[Nothing] 반공변성 [-T]
+
+trait List[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty = true
+  def head = throw new NoSuchElementException("Nil.head")
+  def tail = throw new NoSuchElementException("Nil.tail")
+}
