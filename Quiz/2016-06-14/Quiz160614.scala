c30ec2fd53c2b53f73dbba957e91c67289489b31
LazySoul <myeonginwoo@gmail.com>
2016-07-27 00:30:15 +0900

    Update Quiz160614.scala

--- Quiz/2016-06-14/Quiz160614.scala
+++ Quiz/2016-06-14/Quiz160614.scala
@@ -1,54 +1,34 @@
-import java.io.{File, PrintWriter}
-
-import scala.io.Source
-
 /**
-  * Created by hyungsoklee on 2016. 6. 14..
+  * Created by com on 2016-07-27.
   */
-object Quiz160614 {
 
-  def smallSolution(count: Int, inputList: Array[String]): Unit = {
-    if(inputList.length < 4)
-      return;
+object main {
 
-    val su1 = inputList(0)
-    val symbol = inputList(1)
-    val su2 = inputList(2)
-    val resultSymbol = inputList(3)
-    val result = inputList(4)
-
-    val su1Array = su1.toCharArray.reverse
-    val su2Array = su2.toCharArray.reverse
-    val isPlus = "+".equals(symbol)
-    val resultArray = result.toCharArray.reverse
-
-    var listResult = List()
-    val index = if(su1Array.length < su2Array.length) su1Array.length else su2Array.length
-    for(i <- 0 to index) {
-      // ? 3개
-      if("?".equals(su1Array(i)) && "?".equals(su2Array(i)) && "?".equals(resultArray(i))) {
-      } else
-
-      if("?".equals(su1Array(i)) || "?".equals(su2Array(i))) {
-
-      } else if("?".equals(su1Array(i)) && !"?".equals(su2Array(i))) {
-
-      }
-    }
-
-    println("Case #" + count + ": " + su1 + symbol + su2 + resultSymbol + result)
-  }
+  var primeList = List[Int]()
 
   def main(args: Array[String]) {
-    val filename = "files/B-small-practice.in"
-    val line = Source.fromFile(filename).getLines.toList
+    val min = scala.io.StdIn.readLine().toInt
+    val max = scala.io.StdIn.readLine().toInt
+    println("================================")
+    println(">> min : " + min)
+    println(">> max : " + max)
+    println(">> result : " + getPrimes((2 to max).toList, min))
+  }
 
-    val inputs = line.tail
-    var count = 1
+  def getPrimes(sus: List[Int], min: Int): List[Int] = {
+    sus.filter(isPrime).filter(min < _)
+  }
 
-    inputs.foreach(input => {
-      smallSolution(count, input.split(" "))
-      count += 1
-    })
+  def isPrime(su: Int): Boolean = su match {
+    case 2 =>
+      primeList = List(2)
+      true
+    case _ =>
+      if (primeList.find(su % _ == 0).size == 1) {
+        false
+      } else {
+        primeList = su +: primeList
+        true
+      }
   }
 }
