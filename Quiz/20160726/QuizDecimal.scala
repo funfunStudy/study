64de480806c45855c7bff3b80a10c92d97f0f6f1
Hyungsok Lee <hyungsok7@gmail.com>
2016-07-26 22:25:23 +0900

    Create QuizDecimal.scala

---
+++ Quiz/20160726/QuizDecimal.scala
@@ -0,0 +1,31 @@
+import scala.reflect.internal.settings.MutableSettings
+
+object QuizDecimal {
+
+  var primeList = List[Int]()
+
+  def main(args: Array[String]) {
+    val min = scala.io.StdIn.readLine().toInt
+    val max = scala.io.StdIn.readLine().toInt
+    println("================================")
+    println(">> min : " + min)
+    println(">> max : " + max)
+    println(">> result : " + getPrimes((2 to max).toList, min))
+  }
+
+  def getPrimes(sus: List[Int], min: Int): List[Int] = {
+    sus.filter(isPrime(_)).filter(min < _)
+  }
+
+  def isPrime(su: Int): Boolean = {
+    if (su == 2) {
+      primeList = List(2)
+      true
+    } else if (primeList.find(su % _ == 0).size == 1) {
+      false
+    } else {
+      primeList = su +: primeList
+      true
+    }
+  }
+}
